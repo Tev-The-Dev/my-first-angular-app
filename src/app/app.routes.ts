a949436ba6b254b4d43defc72d59f35317ffb1fc
Tevyn <tevyn.payne@gmail.com>
2024-09-16 17:42:40 -0500

    adding the conact, project and services components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeHomeComponent } from './home-home/home-home.component';
+import { ContactHomeComponent } from './contact-home/contact-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', component: HomeHomeComponent},
+    {path: 'contact', component: ContactHomeComponent},
+];

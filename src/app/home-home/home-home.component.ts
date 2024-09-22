5040e63c823c50f453d226aa68e1abcb171cab40
Tevyn <tevyn.payne@gmail.com>
2024-09-21 21:16:58 -0500

    adding a new project, the calculator

--- src/app/home-home/home-home.component.ts
+++ src/app/home-home/home-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-home',
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './home-home.component.html',
   styleUrl: './home-home.component.css'
 })
-export class HomeHomeComponent {
+export class HomeHomeComponent implements OnInit{
+  bankerName = "Tevyn Payne";
+  bankerAddress = "5000 Dallas";
+  bankerCity = "Euless";
+  bankerState = "TX";
+  bankerZip = "77777";
+  bankerPhone = "(777) 777-7777";
+  bankerEmail = "tevyn.payne@gmail.com";
+  summary = "Eager and motivated software developer with extensive knowledge and hands-on experience in several languages, programming principles, data structures, object-oriented programming algorithms and databases, seeking a challenging role within a reputable organization. Possesses a positive attitude and strong written and verbal communication skills that facilitate effective collaboration. Demonstrates a process optimized logical mindset, drawing from a diverse background that enhances problem-solving and innovation. Committed to contributing positively to organizational goals and fostering a culture of continuous improvement.";
+  school = "Western Govener's University";
+  schoolDates = "09/23 - 08/25";
+  schoolDegree = "Bachelor of Science, Software Engineering";
+  employerName = "Albertsons";
+  jobTitle = "Frozen Foods Department Manager";
+  employmentDates = "09/2023 - Pressent"
 
+  constructor() {}
+  ngOnInit(): void{}
 }
--- src/app/projects-home/projects-home.component.ts
+++ src/app/projects-home/projects-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects-home',
@@ -7,6 +7,122 @@ import { Component } from '@angular/core';
   templateUrl: './projects-home.component.html',
   styleUrl: './projects-home.component.css'
 })
-export class ProjectsHomeComponent {
+export class ProjectsHomeComponent implements OnInit{
+  firstName: string="William";
+  lastName: string="Wallace";
+  phoneNumber: string=" (201) 748-6000";
+  jobTitle: string="Vice President";
+  email: string="willwallace@bank.com";
 
+  zero: number = 0;
+  one: number = 1;
+  two: number = 2;
+  three: number = 3;
+  four: number = 4;
+  five: number = 5;
+  six: number = 6;
+  seven: number = 7;
+  eight: number = 8;
+  nine: number = 9;
+  add: string="+";
+  multiply: string='*';
+  subtract: string ="-";
+  divide: string = "/";
+  clear: string = "C";
+  equals: string = '=';
+  display: string = '0';
+  storeValue: string = '';
+  operator: string = '';
+  operand:  number | null  = null;
+  displayResult: boolean = false;
+
+ 
+  setProperty(event: Event){
+    let target = event.target as HTMLInputElement
+    let property: string = target.id
+    switch(property){
+      case "firstNameInput":
+        this.firstName = target.value;
+        break;
+      case "lastNameInput":
+        this.lastName = target.value;
+        break;
+      case "phoneNumberInput":
+        this.phoneNumber = target.value;
+        break;
+      case "emailInput":
+        this.email = target.value;
+        break;
+      case "jobTitle":
+        this.jobTitle = target.value;
+        break;
+      default:
+        console.warn("No matching ID found!")
+        break;
+    }
+  }
+
+  onClear(event?: MouseEvent){
+    //when user clicks the 'C' button on the calucualter
+    this.display = '0';
+    this.storeValue = '';
+    this.operator = '';
+    this.operand = null;
+    this.displayResult= false;
+  }
+
+  getNum(num: number){
+    //display the numbers being clicked
+    if(this.displayResult){
+      this.storeValue = '';
+      this.displayResult = false;
+    }
+    this.storeValue += num.toString();
+    this.display = this.storeValue;
+  }
+
+  getOperator(op: string){
+    if(this.storeValue){
+      this.operand = parseFloat(this.storeValue)
+      this.operator = op;
+      this.storeValue = '';
+      this.display = this.operator;
+      
+    }
+  }
+  calculate() {
+    if (this.operand !== null && this.storeValue) {
+      const operand2 = parseFloat(this.storeValue);
+      let result: number;
+
+      switch (this.operator) {
+        case '+':
+          result = this.operand + operand2;
+          break;
+        case '-':
+          result = this.operand - operand2;
+          break;
+        case '*':
+          result = this.operand * operand2;
+          break;
+        case '/':
+          result = this.operand / operand2;
+          break;
+        default:
+          return;
+      }
+
+      this.display = result.toString();
+      this.displayResult = true;
+      //this.onClear(); // Reset after calculation
+    }
+  }
+
+
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 }
